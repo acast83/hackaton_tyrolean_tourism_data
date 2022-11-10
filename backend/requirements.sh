f57c89eec5cbe85bccbe3c188be025347b5c00de
aca <aleksandar.stojkovic@digitalcube.rs>
2022-11-10 22:17:50 +0000

    fix

--- backend/requirements.sh
+++
@@ -1,8 +0,0 @@
-pip install --upgrade pip
-
-PYCURL_SSL_LIBRARY=openssl \
-    LDFLAGS="-L$(brew --prefix openssl)/lib" \
-    CPPFLAGS="-I$(brew --prefix openssl)/include" \
-    pip install pycurl --no-cache-dir
-
-cat requirements.txt  | awk '{print "pip install " $0}' | bash
